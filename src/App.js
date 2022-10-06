5dcfc7ef0d7b5df4b200e8cbb38c814839953746
RadoslavKolev <radoslav.kolev02@gmail.com>
2022-10-06 16:29:56 +0300

    1. Creating Custom component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Hello World</h2>
+      <p>Hello Varna</p>
+
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
